af52fd0e87e5d7395048f2d7280c29a41d72fc49
itsspedram <pedmsv@gmail.com>
2025-06-01 15:18:39 +0330

    feat: update ci actions

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,25 @@ const customJestConfig = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'app/**/*.{js,jsx,ts,tsx}',
+    '!app/**/*.d.ts',
+    '!app/**/_*.{js,jsx,ts,tsx}',
+    '!app/**/layout.{js,jsx,ts,tsx}',
+    '!app/**/page.{js,jsx,ts,tsx}',
+    '!app/**/loading.{js,jsx,ts,tsx}',
+    '!app/**/error.{js,jsx,ts,tsx}',
+    '!app/**/not-found.{js,jsx,ts,tsx}',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
